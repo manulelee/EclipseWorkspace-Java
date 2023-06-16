8fba7b0e7ce45dc4ef3376b4be26961ce8206c91
manulelee <emanuele.sy@gmail.com>
2023-06-16 16:45:46 +0200

    push 17:00

--- GestioneIncendi/src/main/java/com/epicode/GestioneIncendiApplication.java
+++ GestioneIncendi/src/main/java/com/epicode/GestioneIncendiApplication.java
@@ -21,17 +21,15 @@ public class GestioneIncendiApplication {
 		Sonda s2 = new Sonda(2, "41.9027835", "12.4963655");
 		Sonda s3 = new Sonda(3, "45.4654219", "9.1859243");
 		
-		s1.addReciver(c1);
-		s2.addReciver(c1);
-		
-		s1.addReciver(c2);
+		s1.addReciver(c1); //Aggiungo la sonda 1 al centro 1
+		s2.addReciver(c1); //Aggiungo la sonda 2 al centro 1
+
+		s1.addReciver(c2); //Aggiungo la sonda 1 al centro 2
 		
 		s1.setSmokeLevel(6); // Verranno richiamate c1 e c2
 		s2.setSmokeLevel(4); // Non verrà richiamata c2 perchè il livello è inferiore a 5
 		s3.setSmokeLevel(4); // s3 non è collegata a nessun cetro di controllo
 		
-
-
 	}
 	
 	}
